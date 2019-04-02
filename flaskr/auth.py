18c360114f8c958b755f3d54a5068890f9f44914
amuqiao <wangqiao@lbesec.com>
2019-04-02 18:43:30 +0800

    添加注册和登录模板

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -15,7 +15,7 @@ from flaskr.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/register', methods=('GET', 'POST'))
+@bp.route('/register/', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -45,7 +45,7 @@ def register():
     return render_template('auth/register.html')
 
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route('/login/', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -83,10 +83,14 @@ def load_logged_in_user():
         ).fetchone()
 
 
-@bp.route('/logout')
+@bp.route('/logout/')
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+# @bp
+# def index():
+#     pass
 
 
 def login_required(view):
