0c8b902aae603b9f4a063bed3be8e68ea70604ca
amuqiao <wangqiao@lbesec.com>
2019-04-03 10:23:28 +0800

    配置项目可安装

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
 # @Time    :2019/4/1 5:21 PM
 # @Author  : wangqiao
+
+
+from setuptools import find_packages, setup
+
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
